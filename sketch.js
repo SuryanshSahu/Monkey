138584a5eafcec7fb6c68d6e78e6cff030e3b201
SuryanshSahu <76650376+SuryanshSahu@users.noreply.github.com>
2021-01-03 21:29:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  monkey=createSprite(50,160,20,50);
+  monkey.addAnimation("running", monkey_running);
+  
+
+  
+}
+
+
+function draw() {
+
+  
+}
+
+
+
+
+
+
